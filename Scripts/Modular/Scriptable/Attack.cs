1e339c9030a984227652c3376282b25a1404c2c4
Can Gök <44663880+cannsky@users.noreply.github.com>
2023-07-08 02:05:26 +0300

    Delete Scripts directory

--- Scripts/Modular/Scriptable/Attack.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Attack", menuName = "Attack")]
-public class Attack : ScriptableObject
-{
-    public GameObject attackVFX;
-    public AudioClip attackSound;
-}
--- Scripts/Scriptable/Attack.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Attack", menuName = "Attack")]
-public class Attack : ScriptableObject
-{
-    public AnimationClip attackAnimation;
-    public GameObject attackVFX;
-    public AudioClip attackSound;
-}
